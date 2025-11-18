be4caec5f40835359ac2792a8888807a3c326e2b
gemkidsacademy <info@gemkidsacademy.com.au>
2025-11-18 17:54:31 +0500

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import os
 from fastapi import Response
 from sqlalchemy import select
 from fastapi.middleware.cors import CORSMiddleware
-from sqlalchemy import create_engine, Column, Integer, String, JSON, DateTime
+from sqlalchemy import create_engine, Column, Integer, String, JSON, DateTime, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 from celery.schedules import crontab
