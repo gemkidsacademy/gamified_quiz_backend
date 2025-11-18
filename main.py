28b5eb4fd0d7189f113ba5fc08087f6431dd2bb2
gemkidsacademy <info@gemkidsacademy.com.au>
2025-11-18 17:21:52 +0500

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from sqlalchemy import create_engine, Column, Integer, String, JSON, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 from celery.schedules import crontab
+import openai
 
 # ---------------------------
 # Database Setup
