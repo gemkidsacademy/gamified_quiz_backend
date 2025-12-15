c28f06a6b4913f6fb879c4761b295933c343bc68
gemkidsacademy <info@gemkidsacademy.com.au>
2025-12-15 20:07:01 +0500

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ from google.cloud import storage
  
 import json
 
-from sqlalchemy import create_engine, Column, Integer, String, JSON, DateTime, ForeignKey, select, func, text, Boolean, Date, Text, desc, Float, Index
+from sqlalchemy import create_engine, Column, Integer, String, JSON, DateTime, ForeignKey, select, func, text, Boolean, Date, Text, desc, Float, Index, case
 from fastapi.responses import JSONResponse
 
 
