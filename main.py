90c2375a93734f6b28a548003799f24827e7178c
gemkidsacademy <info@gemkidsacademy.com.au>
2026-01-22 20:11:12 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2400,7 +2400,7 @@ def get_question_bank_thinking_skills(
         for r in results
     ]
  
-@app.get("/api/reports/student")
+@app.get("/api/reports/student-new")
 def get_student_exam_report(
     student_id: str,
     exam: str,
